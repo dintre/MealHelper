3d5de1d8142b93a50e8f60033816eadd12998ef9
lmaese <43834172+lmaese@users.noreply.github.com>
2018-12-09 00:46:27 -0600

    The basics but need more information to complete

--- FoodQuery.java
+++ FoodQuery.java
@@ -1,8 +1,28 @@
+import java.util.List;
+
 /*
  * runs searches of the BP trees storing food data
  */
 public class FoodQuery {
-
+	private String comparator;
+	private String nutrient;
+	private int nCount;
+	public List<String> results;
+	private BPTree tree;
+	
+	public FoodQuery(String input) {
+		String delims = "[ ]";
+		String[] query = input.split(delims);
+		if(query.length != 3) {
+			throw new IllegalArgumentException("Query must be formatted like 'calories <= 50'");
+		}
+		
+		nutrient = query[0];
+		comparator = query[1];
+		nCount = Integer.parseInt(query[2]);		
+		   
+    }
+    
 	
 	
 	
